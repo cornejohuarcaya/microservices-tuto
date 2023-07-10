4ca3c1bb00deb1a86c65485c9bf930a109172988
Alejo Rodriguez <129014518+alejorcode@users.noreply.github.com>
2023-07-10 11:10:03 -0300

    eureka

--- motorcycle-service/src/main/java/com/motorcycle/service/MotorcycleServiceApplication.java
+++ motorcycle-service/src/main/java/com/motorcycle/service/MotorcycleServiceApplication.java
@@ -2,8 +2,10 @@ package com.motorcycle.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class MotorcycleServiceApplication {
 
 	public static void main(String[] args) {
