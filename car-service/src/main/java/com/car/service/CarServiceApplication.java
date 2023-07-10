9dc13619e89ace84c8023bd0987ddac25c4dae18
Alejo Rodriguez <129014518+alejorcode@users.noreply.github.com>
2023-07-10 12:11:01 -0300

    eureka

--- car-service/src/main/java/com/car/service/CarServiceApplication.java
+++ car-service/src/main/java/com/car/service/CarServiceApplication.java
@@ -2,10 +2,8 @@ package com.car.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableDiscoveryClient
 public class CarServiceApplication {
 
 	public static void main(String[] args) {
--- motorcycle-service/src/main/java/com/motorcycle/service/MotorcycleServiceApplication.java
+++ motorcycle-service/src/main/java/com/motorcycle/service/MotorcycleServiceApplication.java
@@ -2,10 +2,8 @@ package com.motorcycle.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableDiscoveryClient
 public class MotorcycleServiceApplication {
 
 	public static void main(String[] args) {
